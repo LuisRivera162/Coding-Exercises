bfe780a82ddf512fc7fb80249d2eafba08674f80
Luis G. Rivera <luis.rivera162@upr.edu>
2019-09-07 14:13:06 -0400

    Added Solution for the Sum Of Left leaves question

---
+++ IdeaProjects/Programming-Practice/src/LeetCode/Binary_Trees/SumOfLeftLeaves.java
@@ -0,0 +1,33 @@
+// Find the sum of all left leaves in a given binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null)
+            return 0; 
+        return helper(root, root);
+    }
+    
+    public int helper(TreeNode root, TreeNode parent){
+        if(root == null)
+            return 0; 
+        
+        int left = helper(root.left, root);
+        int right = helper(root.right, root);
+        
+        if(root == parent.left && (root.left == null && root.right == null)){
+            return root.val + left + right;
+        }
+        else 
+            return left + right; 
+        
+    }
+}
